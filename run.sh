e89472ca0d7d0ff96736fab4bf9889fedca072be
ArielFMartins <42844932+ArielFMartins@users.noreply.github.com>
2020-10-07 17:01:32 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-sudo chmod +x run.sh;
 wget -c https://github.com/ArielFMartins/Zabbix/raw/main/zabbix.sh;
 wget -c https://github.com/ArielFMartins/Zabbix/raw/main/apache.conf;
 wget -c https://github.com/ArielFMartins/Zabbix/raw/main/zabbix_server.conf;
