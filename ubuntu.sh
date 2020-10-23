e229a3e32232be82cd2d018ccaf540cdb70cbdd1
ArielFMartins <42844932+ArielFMartins@users.noreply.github.com>
2020-10-23 10:58:41 -0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,5 +1,5 @@
 wget -c https://github.com/ArielFMartins/Zabbix/raw/main/ubuntu2004.sh;
 wget -c https://github.com/ArielFMartins/Zabbix/raw/main/apache.conf;
 wget -c https://github.com/ArielFMartins/Zabbix/raw/main/zabbix_server.conf;
-sudo chmod +x ubuntu200.sh;
+sudo chmod +x ubuntu2004.sh;
 sudo ./ubuntu2004.sh
