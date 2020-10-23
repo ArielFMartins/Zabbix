9dea47f98c9cbdfe26fe3fd3f8ce5519988393e5
ArielFMartins <42844932+ArielFMartins@users.noreply.github.com>
2020-10-23 10:57:22 -0300

    Update and rename run.sh to ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,4 @@
-wget -c https://github.com/ArielFMartins/Zabbix/raw/main/zabbix.sh;
+wget -c https://github.com/ArielFMartins/Zabbix/raw/main/ubuntu2004.sh;
 wget -c https://github.com/ArielFMartins/Zabbix/raw/main/apache.conf;
 wget -c https://github.com/ArielFMartins/Zabbix/raw/main/zabbix_server.conf;
 sudo chmod +x zabbix.sh;
